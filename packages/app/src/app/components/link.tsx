afb4821de530bfd624a1f207d4e17fff361dcbb3
Martin Wedvich <wedvich@gmail.com>
2024-08-27 22:22:52 +0200

    feat: add Catalyst UI kit and scaffold login page

---
+++ packages/app/src/app/components/link.tsx
@@ -0,0 +1,14 @@
+import * as Headless from "@headlessui/react";
+import { type LinkProps, Link as RemixLink } from "@remix-run/react";
+import React, { forwardRef } from "react";
+
+export const Link = forwardRef(function Link(
+  props: { href: string | LinkProps["to"] } & Omit<LinkProps, "to">,
+  ref: React.ForwardedRef<HTMLAnchorElement>,
+) {
+  return (
+    <Headless.DataInteractive>
+      <RemixLink {...props} to={props.href} ref={ref} />
+    </Headless.DataInteractive>
+  );
+});
--- packages/app/src/app/root.tsx
+++ packages/app/src/app/root.tsx
@@ -21,7 +21,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-white dark:bg-black">
         {children}
         <ScrollRestoration />
         <Scripts />
